299c6d3dae604068c06be3473860242125a399e8
Susan Gicharu <SGicharu@safaricom.co.ke>
2022-10-07 23:49:56 +0300

    modified js and css

---
+++ js/main.js
@@ -0,0 +1,3 @@
+const dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+const femaleNames= ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
