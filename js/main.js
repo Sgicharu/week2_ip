e9537a0ac8544c1a3fb5d5e4b923757dcf33da6f
Susan Gicharu <SGicharu@safaricom.co.ke>
2022-10-08 16:16:50 +0300

    modified js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
-var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+const dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+const femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
+var century = 19;
+var year = 1989;
+var month = 12;
+var day = 5;
+var gender = "m";
+var indexOfTheDay = (((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7
 
-  }
