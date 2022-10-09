4b0b8b1ecc553f84fea07e77b2821294557667a5
Susan Gicharu <SGicharu@safaricom.co.ke>
2022-10-09 17:03:28 +0300

    mofidied js

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@ var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","S
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
-var century = 19;
-var year = 1989;
-var month = 12;
-var day = 5;
-var gender = "m";
-var indexOfTheDay = (((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7
+function validate() {
+    var genders = document.getElementsByName("gender");
+    if( document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100 || document.myForm.year.value <=1900) {
+       alert( "Please provide a valid year of birth! eg 2019" );
+       document.myForm.year.focus() ;
+       return false;
+    }
 
 indexOfTheDay = Math.round(indexOfTheDay);
 var dayTheUserWasBorn = DAYS_OF_THE_WEEK[indexOfTheDay];
